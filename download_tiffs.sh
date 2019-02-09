c81abc94758ec521fdbaa091f41144164d6899ce
mitochondrion <mitochondrion@users.noreply.github.com>
2019-02-09 16:58:00 -0500

    Changes download script to create and download to './raw/'

--- download_tiffs.sh
+++ download_tiffs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+mkdir -p raw
+
 for value in $(seq -f "%02g" 1 33)
 do
-    curl -O https://pdsimage2.wr.usgs.gov/downloads/MARCI/data_B20/data_resx2/color/B20_day${value}_resx2.tiff
+    (cd ./raw && curl -O https://pdsimage2.wr.usgs.gov/downloads/MARCI/data_B20/data_resx2/color/B20_day${value}_resx2.tiff)
 done
