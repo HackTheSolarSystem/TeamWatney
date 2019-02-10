d7af2cc197b5957f33d2592daa7a347f04bfd72e
James Robert Somers <jamesrobertsomers@gmail.com>
2019-02-10 10:14:28 -0500

    Add parallel downloads

--- download_tiffs.sh
+++ download_tiffs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p raw
+mkdir -p sources
+cd sources
 
-for value in $(seq -f "%02g" 1 33)
+for batch in $(seq -f "%02g" 1 20)
 do
-    (cd ./raw && curl -O https://pdsimage2.wr.usgs.gov/downloads/MARCI/data_B20/data_resx2/color/B20_day${value}_resx2.tiff)
+  seq -f "%02g" 1 33 | parallel -j10 wget -q https://pdsimage2.wr.usgs.gov/downloads/MARCI/data_B${batch}/data_resx2/color/B${batch}_day{1}_resx2.tiff
 done
